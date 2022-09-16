cb36e0536c400102c09d141093ac815c87302078
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-09-16 11:50:15 +0900

    fix: 웹팩 설정 함수 형식으로 변경

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -5,36 +5,38 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-  mode: 'production',
-  output: {
-    filename: 'static/js/[name].[contenthash:8].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
-      },
+module.exports = (_env, prod) => {
+  return {
+    mode: 'production',
+    output: {
+      filename: 'static/js/[name].[contenthash:8].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'static/css/[name].[contenthash:8].css',
+      }),
+      new CleanWebpackPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.API_URL': JSON.stringify(process.env.API_URL),
+      }),
     ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'static/css/[name].[contenthash:8].css',
-    }),
-    new CleanWebpackPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.API_URL': JSON.stringify(process.env.API_URL),
-    }),
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      '...',
-      new CssMinimizerPlugin(),
-    ],
-  },
-};
+    optimization: {
+      minimize: true,
+      minimizer: [
+        '...',
+        new CssMinimizerPlugin(),
+      ],
+    },
+  };
+}
